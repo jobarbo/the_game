5478eb4d02be36ad76b227ae6c281411497eef63
Jonathan Barbeau <Barbeau.jonathan@gmail.com>
2017-05-05 22:38:01 -0400

    Merge branch 'develop'

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,6 +1,7 @@
 var game = {};
 
 game.create = function () {
+  game.physics.startSystem(Phaser.Physics.P2JS);
   var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
   logo.anchor.setTo(0.5, 0.5);
 };
