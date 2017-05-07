0c1a1239cd4b25c2c42aa3e76b776f54f5345478
Jonathan Barbeau <Barbeau.jonathan@gmail.com>
2017-05-07 14:33:31 -0400

    change main.js for mainTitle.js

--- src/js/game/app.js
+++ src/js/game/app.js
@@ -4,7 +4,7 @@ var properties = require('./properties');
 var states = {
   boot: require('./states/boot.js'),
   preloader: require('./states/preloader.js'),
-  game: require('./states/game.js'),
+  game: require('./states/mainTitle.js'),
   level1: require('./states/level1.js')
 };
 
